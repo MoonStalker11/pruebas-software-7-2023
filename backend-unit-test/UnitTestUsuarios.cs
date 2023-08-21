8f055e1d14026d137f5ae7e9a26f4c5b0b76d15b
MoonStalker11 <122495480+MoonStalker11@users.noreply.github.com>
2023-08-20 21:15:12 -0400

    backend v11

---
+++ backend-unit-test/UnitTestUsuarios.cs
@@ -0,0 +1,41 @@
+using backend.connection;
+using backend.entidades;
+using backend.servicios;
+
+namespace backend_unit_test
+{
+    public class UnitTestUsuarios
+    {
+        public UnitTestUsuarios() {
+            BDManager.GetInstance.ConnectionString = "workstation id=UPC-GPS.mssql.somee.com;packet size=4096;user id=Moon11_SQLLogin_1;pwd=qjna5ivvzc;data source=UPC-GPS.mssql.somee.com;persist security info=False;initial catalog=UPC-GPS";
+        }
+        [Fact]
+        public void Usuarios_Get_Verificar_NotNull()
+        {
+            var result = UsuariosServicios.ObtenerTodo<Usuarios>();
+            Assert.NotNull(result); 
+        }
+
+        [Fact]
+        public void Usuarios_GetById_Verificar_Item()
+        {
+            var result = UsuariosServicios.ObtenerById<Usuarios>(1);
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public void Usuarios_Insertar()
+        {
+            Usuarios usuarioTemp = new()
+            {
+                NombreCompleto = "Nombre Test",
+                UserName = "UserName Test",
+                Password = "password Test"
+            };
+
+            var result = UsuariosServicios.InsertUsuario(usuarioTemp);
+            Assert.Equal(1, result);
+        }
+
+    }
+}
